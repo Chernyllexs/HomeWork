8016613c5e49d580059603e42e79766578250d94
Nikita Chernyshov <chernyllexs@mail.ru>
2021-10-20 13:41:35 +0300

    add point class

---
+++ src/com/javaoctoberfest/task/oop_1/MyPoint.java
@@ -0,0 +1,59 @@
+package com.javaoctoberfest.task.oop_1;
+
+public class MyPoint {
+    private int x;
+    private int y;
+
+    public MyPoint() {
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[] getXY(){
+        return new int[] {x, y};
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setXY(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return  Integer.toString(x) + ',' + Integer.toString(y);
+    }
+
+    private double calculateDistance(int x, int y){
+        return Math.sqrt(Math.pow((x - this.x),2) + Math.pow((y - this.y),2));
+    }
+
+    public double distance(int x, int y){
+        return calculateDistance(x, y);
+    }
+
+    public double distance(MyPoint point){
+        return calculateDistance(point.getX(), point.getY());
+    }
+
+    public double distance(){
+        return calculateDistance(0,0);
+    }
+}
