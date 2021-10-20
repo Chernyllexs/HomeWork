323e4344518906eed94a88faa42a7b7ec18baadb
Nikita Chernyshov <chernyllexs@mail.ru>
2021-10-20 12:56:58 +0300

    add book class

---
+++ src/com/javaoctoberfest/task/oop_1/book_class/Book.java
@@ -0,0 +1,64 @@
+package com.javaoctoberfest.task.oop_1.book_class;
+
+import java.util.Arrays;
+
+public class Book {
+    private String name;
+    private Author[] authors;
+    private double price;
+    private int qty = 0;
+
+    public Book(String name, double price, Author... authors) {
+        this.name = name;
+        this.authors = authors;
+        this.price = price;
+    }
+
+    public Book(String name, double price, int qty, Author... authors) {
+        this.name = name;
+        this.authors = authors;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author[] getAuthors() {
+        return authors;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", authors=" + Arrays.toString(authors) +
+                ", price=" + price +
+                ", qty=" + qty +
+                '}';
+    }
+
+    public String getAuthorNames(){
+        StringBuilder stringBuilder = new StringBuilder();
+        for(Author author:authors)
+            stringBuilder.append(author.getName()).append(" ");
+        return stringBuilder.toString();
+    }
+}
