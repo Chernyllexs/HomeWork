a997a74faf7334ca9e844dab523cb0b73d3a13ad
Nikita Chernyshov <chernyllexs@mail.ru>
2021-11-06 16:18:02 +0300

    Add chapter 3 tasks

---
+++ src/com/javaoctoberfest/task/Horstmann/chapter_3/Measurable.java
@@ -0,0 +1,23 @@
+package com.javaoctoberfest.task.Horstmann.chapter_3;
+
+public interface Measurable {
+
+    static double average(Measurable[] objects) {
+        double averageVal = 0;
+        for (Measurable obj : objects)
+            averageVal += obj.getMeasure();
+
+        return averageVal / objects.length;
+    }
+
+    static Measurable largest(Measurable[] objects) {
+        Measurable largest = objects[0];
+        for (Measurable obj : objects) {
+            if (obj.getMeasure() > largest.getMeasure())
+                largest = obj;
+        }
+        return largest;
+    }
+
+    double getMeasure();
+}
